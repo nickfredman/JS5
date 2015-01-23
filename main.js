8148bf7fca220cbe618097355e95e1e42225d70e
Nick Fredmangit config --global ntfredman@gmail.comgit config --global user.name Nick <ntfredman@gmail.com>
2015-01-22 18:01:18 -0700

    First solution, no bonus

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).on('ready', function() {
-  
+    $('textarea').hide();
+    $('p').on('click', function() {
+        var newText = $(this).text();
+        $(this).hide();
+        $(this).siblings('textarea').show();
+        $(this).siblings('textarea').focus();
+        $(this).siblings('textarea').val(newText);
+    });
+
+    $('textarea').blur(function(){
+        var newText = $(this).val();
+        $(this).siblings('p').text(newText);
+        $(this).hide();
+        $(this).siblings('p').show();
+    });
 });
